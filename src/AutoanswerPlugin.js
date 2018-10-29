a2df1c123d904e0bd2d5321ae4047a942cd7e957
Lehel Gyeresi <lgyeresi@twilio.com>
2018-10-29 09:27:25 -0500

    Initial commit

---
+++ src/AutoanswerPlugin.js
@@ -0,0 +1,12 @@
+import { FlexPlugin } from 'flex-plugin';
+import * as Flex from '@twilio/flex-ui';
+
+export default class AutoanswerPlugin extends FlexPlugin {
+  name = 'AutoanswerPlugin';
+
+  init(flex, manager) {
+    manager.workerClient.on("reservationCreated", reservation => {
+      Flex.Actions.invokeAction("AcceptTask", {sid: reservation.sid});
+    });
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import * as FlexPlugin from 'flex-plugin';
+import AutoanswerPlugin from './AutoanswerPlugin';
+
+FlexPlugin.loadPlugin(AutoanswerPlugin);
